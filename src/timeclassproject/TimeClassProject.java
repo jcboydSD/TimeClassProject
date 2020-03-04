61c3aff0c964d0a016046d535904ecbf99e5f2b9
JCBoyd <jcboyd@pm.me>
2020-03-03 21:15:08 -0700

    Initial program shell, commit, and push to remote repository

---
+++ src/timeclassproject/TimeClassProject.java
@@ -0,0 +1,21 @@
+package timeclassproject;
+
+/******************************************************************************
+ * TimeClassProject.java 
+ * Programmer: @author jcboyd
+ * Version: 1.0
+ * Course: SDEV 2210
+ *
+ * This program displays time in various formats
+ *****************************************************************************/
+
+public class TimeClassProject {
+
+    public static void main(String[] args) 
+    {
+
+
+
+    } //end main
+    
+} //end class TimeClassProject
